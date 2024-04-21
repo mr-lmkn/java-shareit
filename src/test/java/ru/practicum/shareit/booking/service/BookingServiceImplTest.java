2d64f51b84699ba31bd818477c9ae961b42a1f55
SLomakin <sergey.lmkn@yandex.ru>
2024-04-21 18:44:50 +0300

    Sprint 15. add some test classes

---
+++ src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.booking.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingServiceImplTest {
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void setState() {
+    }
+
+    @Test
+    void getFromBookerOrOwner() {
+    }
+
+    @Test
+    void getFromUserByRequest() {
+    }
+
+    @Test
+    void isBookingAvailable() {
+    }
+
+    @Test
+    void getStateByUser() {
+    }
+}
---
+++ src/test/java/ru/practicum/shareit/request/service/RequestServiceImplTest.java
@@ -0,0 +1,24 @@
+package ru.practicum.shareit.request.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestServiceImplTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getAllUserItemRequests() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+}
