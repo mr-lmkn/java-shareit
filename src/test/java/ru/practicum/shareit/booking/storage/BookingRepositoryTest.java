1711a3ceceae9e4b6ace0fa086264d8b4afb32f2
SLomakin <sergey.lmkn@yandex.ru>
2024-04-22 20:35:53 +0300

    Sprint 15. BookingsRepo Fix

--- src/test/java/ru/practicum/shareit/booking/storage/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/storage/BookingRepositoryTest.java
@@ -27,32 +27,30 @@ class BookingRepositoryTest {
     @Autowired
     private TestEntityManager entityManager;
 
-    private final long id = 1L;
-    private final long id2 = 2L;
     private final LocalDateTime from = LocalDateTime.now().plusDays(1);
-    private final LocalDateTime to = LocalDateTime.now().plusDays(2);
-    private final User user = User.builder().id(id).email("xxx@mm.eee").build();
+    private final LocalDateTime to = LocalDateTime.now().plusDays(10);
+    private final User user = User.builder().email("xxx@mm.eee").build();
 
-    private final Item item = Item.builder().id(id).owner(user).name("ff").available(true).build();
-    private final Booking booking = Booking.builder().id(id)
+    private final Item item = Item.builder().owner(user).name("ff").available(true).build();
+    private final Booking booking = Booking.builder()
             .start(from).end(to).item(item).booker(user).status(BookingStatus.CANCELED).build();
 
     @Test
     void updateStatus() {
-        userRepo.save(user);
-        itemRepo.save(item);
-        bookingRepository.save(booking);
-        bookingRepository.updateStatus(1, id, id);
+        User userX = userRepo.save(user);
+        Item itemX = itemRepo.save(item);
+        Booking bookingX = bookingRepository.save(booking);
+        bookingRepository.updateStatus(1, bookingX.getId(), userX.getId());
         entityManager.clear();
-        BookingStatus state = bookingRepository.findById(id).get().getStatus();
+        BookingStatus state = bookingRepository.findById(bookingX.getId()).get().getStatus();
         assertEquals(1, state.ordinal());
     }
 
     @Test
     void isBookingAvailable() {
-        userRepo.save(user);
+        User userX = userRepo.save(user);
         Item itemX = itemRepo.save(item);
-        bookingRepository.save(booking);
+        Booking bookingX = bookingRepository.save(booking);
         entityManager.clear();
         assertEquals(true,
                 bookingRepository.isBookingAvailable(itemX.getId(), from.plusDays(10), to.plusDays(11)));
@@ -60,13 +58,18 @@ class BookingRepositoryTest {
 
     @Test
     void isBookingAvailable_ERR() {
-        userRepo.save(user);
-        Item itemX = itemRepo.save(item);
-        bookingRepository.save(booking);
-        bookingRepository.updateStatus(1, itemX.getId(), id);
-        entityManager.getEntityManager().clear();
+        User owner = userRepo.save(User.builder().email("d").build());
+        User userX = userRepo.save(user);
+        Item itemX = itemRepo.save(Item.builder().owner(owner).build());
+        Booking bookingX = bookingRepository.save(
+                Booking.builder().booker(userX)
+                        .item(itemX)
+                        .start(from).end(to)
+                        .status(BookingStatus.APPROVED).build());
+        entityManager.clear();
+        System.out.println(bookingX.toString());
         assertEquals(false,
-                bookingRepository.isBookingAvailable(itemX.getId(), from, to));
+                bookingRepository.isBookingAvailable(itemX.getId(), from.minusDays(1), to.minusDays(5)));
     }
 
 
@@ -77,14 +80,13 @@ class BookingRepositoryTest {
         Item itemX = itemRepo.save(Item.builder().owner(owner).build());
         Booking b = bookingRepository.save(Booking
                 .builder().booker(userX).item(itemX).status(BookingStatus.APPROVED)
-                .start(from.plusDays(-10)).end(to.plusDays(-9))
+                .start(from.plusDays(-100)).end(to.plusDays(-90))
                 .build());
         Booking b2 = bookingRepository.save(Booking
                 .builder().booker(userX).item(itemX).status(BookingStatus.APPROVED)
-                .start(from.plusDays(-5)).end(to.plusDays(-4))
+                .start(from.plusDays(-50)).end(to.plusDays(-40))
                 .build());
         entityManager.getEntityManager().clear();
-        System.out.println(booking.toString());
         assertEquals(1,
                 bookingRepository.getFromUserByStatePage(userX.getId(),"PAST", false, 1,1).size()
         );
@@ -97,11 +99,11 @@ class BookingRepositoryTest {
         Item itemX = itemRepo.save(Item.builder().owner(owner).build());
         Booking b = bookingRepository.save(Booking
                 .builder().booker(userX).item(itemX).status(BookingStatus.APPROVED)
-                .start(from.plusDays(-10)).end(to.plusDays(-9))
+                .start(from.plusDays(-100)).end(to.plusDays(-90))
                 .build());
         Booking b2 = bookingRepository.save(Booking
                 .builder().booker(userX).item(itemX).status(BookingStatus.APPROVED)
-                .start(from.plusDays(-5)).end(to.plusDays(-4))
+                .start(from.plusDays(-50)).end(to.plusDays(-40))
                 .build());
         entityManager.getEntityManager().clear();
         System.out.println(booking.toString());
